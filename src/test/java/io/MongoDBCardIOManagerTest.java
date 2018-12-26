e301a49fe56eeeb2519550d48004777decae8e68
Colin Redman <20376935+credman0@users.noreply.github.com>
2018-12-25 17:19:38 -0700

    Implemented mongodb card io

---
+++ src/test/java/io/MongoDBCardIOManagerTest.java
@@ -0,0 +1,60 @@
+package io;
+
+import core.Card;
+import core.Cite;
+import io.cardio.CardIOManager;
+import io.cardio.CardSplitFiles.CardSplitFilesStreamer;
+import io.cardio.mongodb.MongoDBCardIOManager;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.*;
+
+class MongoDBCardIOManagerTest {
+    Card card;
+    Card card2;
+
+    @BeforeClass
+    public void setUp(){
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("testCardText.txt").getFile());
+        String text = null;
+        try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))){
+            text = new String(in.readAllBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        card = new Card(new Cite("Smith", "2010", "Renowned writer of cards"), text);
+        card2 = new Card(new Cite("Smith", "2010", "Renowned writer of cards"), text+"AAA");
+    }
+
+    @Test
+    public void writeReadTest (){
+        try (CardIOManager manager = new MongoDBCardIOManager()) {
+            manager.storeCard(card);
+            Card recoveredCard = manager.retrieveCard(card.getHash());
+            Assert.assertEquals(card.getText(), recoveredCard.getText());
+            manager.storeCard(card);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void doubleWriteReadTest(){
+        try (CardIOManager manager = new MongoDBCardIOManager()) {
+            manager.storeCard(card);
+            manager.storeCard(card2);
+            Card recoveredCard = manager.retrieveCard(card.getHash());
+            Card recoveredCard2 = manager.retrieveCard(card2.getHash());
+            Assert.assertEquals(card.getText(), recoveredCard.getText());
+            Assert.assertEquals(card2.getText(), recoveredCard2.getText());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
